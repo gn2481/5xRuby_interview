2c7b555cde25734ffc998dd9df48eebfbfb398cf
yuan <gn2481@gmail.com>
2020-12-03 14:22:55 +0800

    add_views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,32 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validate :password_comfirm
+  before_create :encrypt_password
+
+  def self.login(options)
+    if options[:account] && options[:password]
+      find_by(account: options[:account],
+              password: Digest::SHA1.hexdigest("yu" + options[:password] +"an"))
+    end
+  end
+
+  private
+  def encrypt_password
+    self.password = bigbang(self.password)
+    self.comfirmation = bigbang(self.comfirmation)
+  end
+
+  def bigbang(string)
+    string = "yu" + string + "an"
+    Digest::SHA1.hexdigest(string)
+  end
+
+  def password_comfirm
+    if self.password == self.comfirmation
+      true
+    else
+      false
+    end
+  end
 end
